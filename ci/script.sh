cde6f80d3a136b8a8f5d15d10d4dc0192aa8deb1
Jorge Aparicio <jorge@japaric.io>
2019-01-03 09:31:53 +0100

    add microamp and AMP example

--- ci/script.sh
+++ ci/script.sh
@@ -4,8 +4,36 @@ main() {
     case $TARGET in
         arm*v7r-none-eabi*)
             pushd firmware/zup-quickstart
-            cargo build --examples
-            cargo build --examples --release
+
+            # single-core examples
+            local examples=(
+                abort
+                hello
+                icdicer
+                icdipr
+                lock
+                nested
+                panic
+                rtfm-lock
+                rtfm-message
+                sgi
+            )
+
+            for ex in ${examples[@]}; do
+                cargo build --example $ex
+                cargo build --example $ex --release
+            done
+
+            # multi-core examples
+            examples=(
+                amp
+            )
+
+            for ex in ${examples[@]}; do
+                cargo amp --example $ex
+                cargo amp --example $ex --release
+            done
+
             ;;
         *)
             pushd firmware/zup-rt
